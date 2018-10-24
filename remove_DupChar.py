d8ab99f652cb2ca26756d3621222fd2c2145f31d
Feronial <mistiq.sonko@gmail.com>
2018-10-24 08:56:59 +0300

    Create remove_DupChar.py

---
+++ remove_DupChar.py
@@ -0,0 +1,9 @@
+def removeDupes(string):
+    empty_Str = ""
+    
+    for i in string:
+        
+        if ch not in empty_Str:
+            empty_Str = empty_Str + ch
+    
+    return empty_Str
