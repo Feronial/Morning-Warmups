56cbac359c4a16588acbd2460ca9d838428decc9
Feronial <mistiq.sonko@gmail.com>
2018-10-25 12:03:47 +0300

    Update median.py

--- median.py
+++ median.py
@@ -1,14 +1,16 @@
 #Find median of list and index of it
 
 def median(int_List):
+    
     int_List.sort()
+    
     if len(int_List) % 2 == 0:
         
         return sum(int_List[int((len(int_List) / 2) - 1) : int(len(int_List) / 2) + 1 ] ) / 2
         
     else:
         
-        return int_List[round(len(int_List)/2)]
+        return int_List[int(len(int_List)/2)]
     
 def median_Index(int_List):
     
@@ -18,5 +20,6 @@ def median_Index(int_List):
         
     else:
         
-        return round(len(int_List)/2)
+        return int(len(int_List)/2)
+    
     
