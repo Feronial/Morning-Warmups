96af8cf8a4265c34890065ddb0e6c5c26af4953d
Feronial <mistiq.sonko@gmail.com>
2018-10-25 10:02:14 +0300

    Create median.py

---
+++ median.py
@@ -0,0 +1,11 @@
+#Find median of list
+
+def median(int_List):
+    
+    if len(int_List) % 2 == 0:
+        int_List.sort()
+        return sum(int_List[int((len(int_List) / 2) - 1) : int(len(int_List) / 2) + 1 ] ) / 2
+    
+    else:
+        
+        return int_List[round(len(int_List)/2)]
